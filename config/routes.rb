ac309e4aed26b3d4890398038d7e1c66d92b5c24
Luong Viet Dung <luong.viet.dung@framgia.com>
2015-08-06 14:07:58 +0700

    Finish forgot password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: "registrations"}
   root "books#index"
   get "about", to: "static_pages#about"
 
